688879ffda95f3ede453e489ce1f3a0494418015
Balasubramanian1999 <absmani1999@gmail.com>
2023-03-30 20:46:59 +0530

    new file add

---
+++ main.js
@@ -0,0 +1,22 @@
+//switch
+
+var a=parseInt (prompt ("enter value "));
+	b=parseInt (prompt ("enter value"));
+	c=prompt ("enter");
+	
+	switch(c) {
+		
+		case"1": console.log (a+b);
+		break;
+		case"2": console.log (a-b);
+		break;
+		case"3": console.log (a*b);
+		break;
+		case"4": console.log (a**b);
+		break;
+		case"5": console.log (a/b);
+		break;
+		default: console.log ("wroung no")
+		break;
+	}
+	
